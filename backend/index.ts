156bc796fa67e1150a376e18f98d54398505bab2
cecepizza <pizzacort@iivi.co>
2024-12-17 11:07:39 -0500

    server up

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express();
+const port = 8000;
+
+app.get("/", (req, res) => {
+  res.send("wut is up yo");
+});
+app.listen(port, () => {
+  console.log(`server is a success on http://localhost:${port}`);
+});
