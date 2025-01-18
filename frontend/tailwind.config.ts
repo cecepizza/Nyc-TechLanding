c345be3cf92691d222cc38e2b20cce25e9a3c3aa
cecepizza <pizzacort@iivi.co>
2025-01-18 15:17:43 -0500

    add built by FT tag

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -46,12 +46,18 @@ const config: Config = {
   				to: {
   					'background-position': '0% 0%'
   				}
+  			},
+  			'border-beam': {
+  				'100%': {
+  					'offset-distance': '100%'
+  				}
   			}
   		},
   		animation: {
   			marquee: 'marquee var(--duration) infinite linear',
   			'marquee-vertical': 'marquee-vertical var(--duration) linear infinite',
-  			shine: 'shine var(--duration) infinite linear'
+  			shine: 'shine var(--duration) infinite linear',
+  			'border-beam': 'border-beam calc(var(--duration)*1s) infinite linear'
   		}
   	}
   },
